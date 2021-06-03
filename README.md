ac3d03da25977c68064d1a76ae6528139954bf58
Augusto Zanellato <augusto.zanellato@gmail.com>
2021-06-03 16:55:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# VaxBot9000
+
+## Prerequisites
+- Python 3.9
+- [Poetry](https://python-poetry.org/)
+- [latest version of geckodriver](https://github.com/mozilla/geckodriver/releases/tag/v0.29.1)
+- Firefox
+
+## Setup
+1. `poetry install`
+1. Copy `config.sample.toml` to `config.toml` and edit the following parameters:
+    - `az` your local ULSS (example `ulss6`)
+    - `cf` your Italian fiscal code
+    - `num_team` last six digits of your TEAM card
+    - `use_tg` wheter to use Telegram to send notifications
+    - `tg_token` your Telegram bot token
+    - `tg_chat` your Telegram chat_id or the username of the channel where you wish to send the alerts
+    - `spawn_new_browser_on_slot_found` wheter to spawn a new browser window every time a slot is found. **WARNING: this can get pretty chaotic**
+
+## Running
+`poetry run python -m vaxbot`
---
+++ vaxbot/__main__.py
@@ -0,0 +1,41 @@
+from splinter import Browser
+from telegram import Bot
+from time import sleep
+import logging
+import toml
+import os.path
+
+config = toml.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'config.toml'))
+bot = None
+if config['use_tg']:
+    bot = Bot(config["tg_token"])
+browser = Browser('firefox', headless=not config['spawn_new_browser_on_slot_found'])
+newline = '\n'
+
+
+def fill_browser(browser):
+    global config
+    browser.visit(f'https://vaccinicovid.regione.veneto.it/{config["az"]}')
+    browser.fill('cod_fiscale', config["cf"])
+    browser.fill('num_tessera', config["num_team"])
+    browser.find_by_css('div.form-group:nth-child(4) > div:nth-child(2) > input:nth-child(1)').first.check()
+    browser.evaluate_script('inviacf()')
+
+def send_message(text):
+    if config['use_tg']:
+        bot.send_message(config['tg_chat'], text)
+
+old_slots = []
+while True:
+    fill_browser(browser)
+    available_slots = [button.html.split('<br>')[0].strip() for button in browser.find_by_css('#corpo2 button.btn-primary:not(.btn-back') if 'onclick' in button.outer_html]
+    if available_slots != old_slots:
+        print(f'slot change: {available_slots}')
+        if len(available_slots) == 0:
+            send_message(f'Slot terminati!')
+        else:
+            if config["spawn_new_browser_on_slot_found"]:
+                browser = Browser('firefox')
+            send_message(f'Slot disponibili:{newline}{newline.join(available_slots)}')
+    old_slots = available_slots
+    sleep(0.1)
